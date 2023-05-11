e6861e9f44273e83202ce34aa25fab0246f006c7
codersuresh <sd807141@gmail.com>
2023-05-11 09:54:48 +0545

    store in localStorage if offline and push when online

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getDatabase, ref, onValue, push } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 
+let dbRef = ""
 if (navigator.onLine) {
-
     const firebaseConfig = {
         databaseURL: "https://endorsement-app-4f276-default-rtdb.asia-southeast1.firebasedatabase.app/",
     }
@@ -10,74 +10,94 @@ if (navigator.onLine) {
     const app = initializeApp(firebaseConfig);
     const database = getDatabase(app);
 
-    const dbRef = ref(database, "/endorsements");
+    dbRef = ref(database, "/endorsements");
+}
 
-    const msgInput = document.getElementById("endorsement");
-    const fromInput = document.getElementById("from");
-    const toInput = document.getElementById("to");
-    const submitBtn = document.getElementById("submit");
-    const endorsementContainer = document.getElementById("endorsements");
-    const form = document.getElementById("form");
+const msgInput = document.getElementById("endorsement");
+const fromInput = document.getElementById("from");
+const toInput = document.getElementById("to");
+const submitBtn = document.getElementById("submit");
+const endorsementContainer = document.getElementById("endorsements");
+const form = document.getElementById("form");
 
-    submitBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (form.checkValidity()) {
-            addEndorsement();
-        } else {
-            form.reportValidity();
-        }
-    });
-
-    const endorsements = JSON.parse(localStorage.getItem("endorsements")) || [];
-
-    function addEndorsement() {
-        const endorsement = {
-            msg: msgInput.value,
-            from: fromInput.value,
-            to: toInput.value
-        };
-
-        endorsements.push(endorsement);
-
-        push(dbRef, endorsement);
-
-        localStorage.setItem("endorsements", JSON.stringify(endorsements));
-
-        form.reset();
-        renderEndorsements();
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (form.checkValidity()) {
+        addEndorsement();
+    } else {
+        form.reportValidity();
     }
+});
 
-    // Listen for new endorsements in the database
-    onValue(dbRef, (snapshot) => {
-        const data = Object.values(snapshot.val());
+const endorsements = JSON.parse(localStorage.getItem("endorsements")) || [];
 
-        if (data && data.length > 0) {
-            renderEndorsements(data);
-        } else {
-            endorsementContainer.textContent = "No endorsements";
-        }
-    });
+function addEndorsement() {
+    const endorsement = {
+        msg: msgInput.value,
+        from: fromInput.value,
+        to: toInput.value
+    };
 
-    function renderEndorsements() {
-        if (endorsements && endorsements.length > 0) {
-            endorsementContainer.textContent = "";
-            endorsements.forEach(endorsement => {
-                const endorsementDiv = document.createElement("div");
-                endorsementDiv.classList.add("endorsement");
+    endorsements.push(endorsement);
 
-                endorsementDiv.innerHTML = `
+    if (navigator.onLine) {
+        push(dbRef, endorsement);
+    }
+    localStorage.setItem("endorsements", JSON.stringify(endorsements));
+
+    form.reset();
+    getEndorsements(endorsements);
+}
+
+getEndorsements(endorsements);
+
+function getEndorsements() {
+    // Listen for new endorsements
+    if (navigator.onLine) {
+        onValue(dbRef, (snapshot) => {
+            const data = snapshot.val() ? Object.values(snapshot.val()) : [];
+
+            if (data && data.length > 0) {
+                renderEndorsements(data);
+            } else {
+                endorsementContainer.textContent = "No endorsements";
+            }
+        });
+    } else {
+        renderEndorsements(endorsements);
+    }
+}
+
+// push local data to firebase
+if (endorsements.length) {
+    if (confirm("You have endorsements in local storage. Do you want to push them to the database?")) {
+        endorsements.forEach((endorsement, i) => {
+            if (endorsement.msg != "" ||
+                endorsement.from != "" ||
+                endorsement.to != "") {
+                push(dbRef, endorsement)
+            }
+        });
+        localStorage.removeItem("endorsements");
+    }
+}
+
+function renderEndorsements(endorsements) {
+    if (endorsements && endorsements.length > 0) {
+        endorsementContainer.textContent = "";
+        endorsements.forEach(endorsement => {
+            const endorsementDiv = document.createElement("div");
+            endorsementDiv.classList.add("endorsement");
+
+            endorsementDiv.innerHTML = `
                     <h3>From: ${endorsement.from}</h3>
                     <p>${endorsement.msg}</p>
                     <h3>To: ${endorsement.to}</h3>
             `;
 
-                endorsementContainer.appendChild(endorsementDiv);
-            });
-        } else {
-            endorsementContainer.textContent = "No endorsements";
-        }
+            endorsementContainer.appendChild(endorsementDiv);
+        });
+    } else {
+        endorsementContainer.textContent = "No endorsements";
     }
-
-} else {
-    document.getElementById("offline").style.display = "block";
 }
